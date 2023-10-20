93a8845c15b081b81e9bb4864ef790c1a0612312
CarlosBaez11 <70594075+CarlosBaez11@users.noreply.github.com>
2023-10-20 09:38:45 -0500

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,40 +1,22 @@
-job('ejemplo2-job-DSL') {
-	description('Job DSL de ejemplo para el curso de Jenkins')
-  	scm {
-      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
-      		}
-    	} 
-  	parameters {
-   		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
-      		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
-      		booleanParam('agente', false)
-    	}
-  	triggers {
-    		cron('H/7 * * * *')
-    	}
-  	steps {
-    		shell("bash jobscript.sh")
-    	}
-  	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
-      		slackNotifier {
-		  notifyAborted(true)
-		  notifyEveryFailure(true)
-		  notifyNotBuilt(false)
-		  notifyUnstable(false)
-		  notifyBackToNormal(true)
-		  notifySuccess(false)
-		  notifyRepeatedFailure(false)
-		  startNotification(false)
-		  includeTestSummary(false)
-		  includeCustomMessage(false)
-		  customMessage(null)
-		  sendAs(null)
-		  commitInfoChoice('NONE')
-		  teamDomain(null)
-		  authToken(null)
-        	}
-    	}
+job("Example_job_DSL"){
+	description("Job DSL Exercise")
+  scm{
+    git("https://github.com/macloujulian/jenkins.job.parametrizado.git", "main"){node ->
+      
+      node / gitConfigName("CarlosBaez11")
+      
+      node / gitConfigEmail("eduardo.baez@udea.edu.co")
+      }
+    parameters{
+      stringParam("nombre", defaultValue = "Eduardo", description = "Parámetro de cadena para Job")
+      
+      choiceParam("Planeta", ["Mercurio", "Tierra (default)", "Marte"])
+      
+      booleanParam("agente", false)
+    }
+    publishers{
+      mailer("carlos.baez@ceiba.com.co", true, true)
+    
+    }
+}
 }
